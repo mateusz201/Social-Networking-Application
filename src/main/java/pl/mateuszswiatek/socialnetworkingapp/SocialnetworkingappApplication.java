b741b438c6c2b1e9cd83f3154d16682e44375c10
Mateusz <thedarksoliderpl1@gmail.com>
2024-03-16 16:40:55 +0100

    chore: project setup

---
+++ src/main/java/pl/mateuszswiatek/socialnetworkingapp/SocialnetworkingappApplication.java
@@ -0,0 +1,12 @@
+package pl.mateuszswiatek.socialnetworkingapp;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialnetworkingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialnetworkingappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/mateuszswiatek/socialnetworkingapp/SocialnetworkingappApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mateuszswiatek.socialnetworkingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialnetworkingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
