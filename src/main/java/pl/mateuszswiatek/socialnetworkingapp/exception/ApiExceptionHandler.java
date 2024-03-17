86a24b53bcc88e1355f16ee0677d8aa84a0c19fc
Mateusz <thedarksoliderpl1@gmail.com>
2024-03-17 20:32:34 +0100

    chore: add custom responses for validation and type mismatch exceptions

--- src/main/java/pl/mateuszswiatek/socialnetworkingapp/exception/ApiExceptionHandler.java
+++ src/main/java/pl/mateuszswiatek/socialnetworkingapp/exception/ApiExceptionHandler.java
@@ -1,11 +1,13 @@
 package pl.mateuszswiatek.socialnetworkingapp.exception;
 
 
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -17,7 +19,7 @@ import static org.springframework.http.HttpStatus.BAD_REQUEST;
 @RestControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(ApiException.class)
-    public ResponseEntity<ApiExceptionResponse> handleApiException(ApiException exception){
+    public ResponseEntity<ApiExceptionResponse> handleApiException(ApiException exception) {
         ApiExceptionResponse body = new ApiExceptionResponse(exception.getMessage());
         HttpStatus status = exception.getStatus();
 
@@ -32,6 +34,28 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
             WebRequest request
     ) {
         ApiExceptionResponse body = new ApiExceptionResponse("Request body is not readable");
+        return new ResponseEntity<>(body, BAD_REQUEST);
+    }
+
+    @Override
+    public ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request
+    ) {
+        ApiExceptionResponse body = new ApiExceptionResponse("Some parameters are not valid");
+        return new ResponseEntity<>(body, BAD_REQUEST);
+    }
+
+    @Override
+    public ResponseEntity<Object> handleTypeMismatch(
+            TypeMismatchException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request
+    ) {
+        ApiExceptionResponse body = new ApiExceptionResponse("Path variable not valid");
         return new ResponseEntity<>(body, BAD_REQUEST);
     }
 }
